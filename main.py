9b8cfda9170318368511d079830cce7b6f0422a0
Matthew <matthewjwright07@gmail.com>
2025-05-02 15:22:58 -0500

    updates

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import random
+import time
 
-words = ["hello","the","no","yes","maybe","never","why","awesome","super","wow","go","get","my","your"]
+words = [
+    "the", "be", "to", "of", "and", "a", "in", "that", "have", "I",
+    "it", "for", "not", "on", "with", "he", "as", "you", "do", "at",
+    "this", "but", "his", "by", "from", "they", "we", "say", "her", "she"
+]
 
 word_list = []
 
-for _ in range(15):
+number_of_words = int(input("How many words would you like?\n"))
+
+for _ in range(number_of_words):
     word_list.append(random.choice(words))
 
 string_words = ' '.join(word_list)
 
-print(string_words)
+start_time = time.time()
 
-user_input = input("type out the letters\n")
+user_input = input(f"{string_words}\n")
+
+end_time = time.time()
+
+final_time = round(end_time-start_time,2)
+wpm = round(number_of_words/final_time*60,2)
 
 if user_input==string_words:
-    print("Yay you did it")
+    print(f"You completed {number_of_words} words in {final_time} seconds or {wpm} words per minute")
 else:
-    print("Your trash kid")
+    print("You have failed the test try again.")
 
